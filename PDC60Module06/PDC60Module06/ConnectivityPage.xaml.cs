f57d5ddd92b9472f72308c73065d6f7f259de644
Nicole Frankie Capuno <colecapuno@gmail.com>
2023-11-10 12:18:05 +0800

    initial commit

---
+++ PDC60Module06/PDC60Module06/ConnectivityPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC60Module06
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConnectivityPage : ContentPage
+    {
+        public ConnectivityPage()
+        {
+            InitializeComponent();
+        }
+        private void Connectivity_Clicked(object sender, EventArgs e)
+        {
+            if (Connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+
+                DisplayAlert("NO Internet", "Please Connect to an Internat", "Ok");
+                return;
+            }
+        }
+    }
+}
---
+++ PDC60Module06/PDC60Module06/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC60Module06
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Accelerometer_Page_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AccelerometerPage());
+        }
+        private async void Connectivity_Page_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ConnectivityPage());
+        }
+        private async void Device_Battery_Page_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DeviceBatteryPage());
+        }
+    }
+}
